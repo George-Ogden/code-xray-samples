0c1aff9d93ca8e9dccda111486e8b6681aa88479
George Ogden <george-ogden@outlook.com>
2024-09-15 13:44:57 +0100

    add new test
    base case for quicksort

--- quicksort_test.py
+++ quicksort_test.py
@@ -4,3 +4,8 @@ from quicksort import quicksort
 def test_quicksort():
     arr = [12, 4, 5, 6, 7, 3, 1, 15]
     assert sorted(arr.copy()) == quicksort(arr)
+
+
+def test_quicksort_base_case():
+    arr = [5]
+    assert quicksort(arr) == [5]
